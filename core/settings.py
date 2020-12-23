f2b4ab49cbb5b7e71372d0aecdaf25702799039e
luisa <luisafer.11@hotmail.com>
2020-12-22 20:01:32 -0500

    Acerca de

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','150.136.241.89', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
