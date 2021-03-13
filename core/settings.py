6f390baa3f79fdc0f9f625879d3283ad27e0d398
Victor <victord.castanedap@utadeo.edu.co>
2021-03-13 16:30:52 -0500

    Change user and password database

--- core/settings.py
+++ core/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'smood',
-        'USER': 'demo',
-        'PASSWORD': 'demo',
+        'USER': 'smood',
+        'PASSWORD': 'smood',
         'HOST': 'localhost',
         'PORT': '',
     }
